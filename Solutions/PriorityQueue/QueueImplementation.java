3de6e5b588dc29a614faccdec50f19cde90fe108
jayitasaha40 <jayitasaha40@gmail.com>
2024-07-23 10:05:28 +0530

    Added QueueImplementation

---
+++ Solutions/PriorityQueue/QueueImplementation.java
@@ -0,0 +1,44 @@
+//Implement a Queue using the PriorityQueue class with a custom comparator.
+
+import java.io.*;
+import java.util.*;
+
+ class Element{
+    String s;
+    int index;
+    
+    public Element(String s,int index){
+        this.s = s;
+        this.index = index;
+    }
+}
+class QueueImplementation {
+	public static void main (String[] args) {
+	    //*** Taking Input ***
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); //No of elements
+		
+// 		Creating Custom Comparator
+		Comparator<Element> comp = new Comparator<>(){
+		    public int compare(Element a, Element b){
+		        return Integer.compare(a.index,b.index);
+		    }
+		};
+		
+		//Creating a PriorityQueue using custom comparator
+		PriorityQueue<Element> pq = new PriorityQueue<>(comp);
+        
+		//Adding Input elements into it
+		for(int i = 0;i<n;i++){
+		    pq.add(new Element(sc.next(),i));
+		}
+		
+		sc.close();
+		
+		//*** Printing the Elements in Sorted Order ***
+		while(!pq.isEmpty()){
+		    Element ele = pq.poll();
+		    System.out.println(ele.index+"-> "+ele.s);
+		}
+	}
+}
